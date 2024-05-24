700f4bb212aaa7a6d8bfd3bc528f3a79640bdaf5
Kristiyan <74624512+krisg9@users.noreply.github.com>
2024-05-24 14:38:12 +0200

    PP-24 - product details page
    * PP-24 product details page
    
    * PP-24 WIP adjust style
    
    * PP-24 product details page
    
    * Removed unused loader function in root page
    
    * Corrected the expected response type in the product details loader function
    
    ---------
    
    Co-authored-by: clzander <cle.zander@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
-	],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				btn: "#99AFFF",
+				btnFocused: "#6B7AB2",
+			},
+		},
 	},
-	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 
 	// daisyUI config
 	daisyui: {
 		themes: [
 			{
 				perrfectlypawsh: {
-					"primary": "#FED363",
-					"secondary": "#FFE7AA",
+					primary: "#FED363",
+					secondary: "#FFE7AA",
 					"base-100": "#FFF4D6",
-					"info-content": "9B9B9B",
+					"info-content": "#9B9B9B",
 				},
 			},
 		],
-    darkTheme: "dark", // name of one of the included themes for dark mode
+		darkTheme: "dark", // name of one of the included themes for dark mode
 		base: true, // applies background color and foreground color for root element by default
 		styled: true, // include daisyUI colors and design decisions for all components
 		utils: true, // adds responsive and modifier utility classes
@@ -29,5 +31,4 @@ export default {
 		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
 		themeRoot: ":root", // The element that receives theme color CSS variables
 	},
-}
-
+};
