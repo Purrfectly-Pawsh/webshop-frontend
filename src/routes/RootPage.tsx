4bbfe66f5a31bfd48bb8b6f4d9c1fcb670bd918a
clzander <cle.zander@gmail.com>
2024-05-24 14:24:44 +0200

    Removed unused loader function in root page

--- src/routes/RootPage.tsx
+++ src/routes/RootPage.tsx
@@ -1,14 +1,7 @@
-import { Outlet, redirect } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import CategoryMenu from "../components/CategoryMenu";
 import Footer from "../components/Footer";
-
-export const loader = async () => {
-	console.log("Loader entered");
-	// as long as we don't have a home page, we are just going to redirect
-	// users if they want to access "/"
-	return redirect("/products");
-};
 
 export default function RootPage() {
 	return (
