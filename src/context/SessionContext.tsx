4ca646a91ec3763adf7bd6e14c0c309a42826aca
clzander <cle.zander@gmail.com>
2024-06-14 16:37:17 +0200

    Implemented the auth context

--- src/context/SessionContext.tsx
+++ src/context/SessionContext.tsx
@@ -1,12 +1,44 @@
-import { type ReactNode, createContext, useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import { type ReactNode, createContext, useState, useEffect } from "react";
+import { useAuth } from "react-oidc-context";
 import { v4 as uuidv4 } from "uuid";
 
 interface SessionContextType {
 	getBasketId: () => string;
+	signinRedirect: () => Promise<void>;
+	signoutSilent: () => Promise<void>;
+	isLoading: boolean;
+	user: User;
+}
+
+interface CustomDecodedToken {
+    resource_access: {
+        purrfectly_pawsh: {
+            roles: string[];
+        };
+    };
+}
+
+interface User {
+	authenticated: boolean;
+	roles: string[];
+	isAdmin: boolean;
+	isUser: boolean;
+}
+
+const guest = {
+	authenticated: false,
+	roles: [],
+	isAdmin: false,
+	isUser: false,
 }
 
 export const SessionContext = createContext<SessionContextType>({
 	getBasketId: () => "",
+	signinRedirect: async () => {},
+	signoutSilent: async () => {},
+	isLoading: false,
+	user: guest,
 });
 
 interface SessionContextProviderProps {
@@ -20,6 +52,26 @@ export const SessionContextProvider = ({
 		const storedBasketId = localStorage.getItem("basketId");
 		return storedBasketId || "";
 	});
+	const auth = useAuth();
+	const [user, setUser] = useState<User>(guest);
+	
+	useEffect(() => {
+		if (auth.isAuthenticated && auth.user) {
+			try {
+				const decoded = jwtDecode<CustomDecodedToken>(auth.user.access_token);
+			const user: User = {
+				authenticated: true,
+				roles: decoded.resource_access.purrfectly_pawsh.roles,
+				isAdmin: decoded.resource_access.purrfectly_pawsh.roles.includes("ADMIN"),
+				isUser: decoded.resource_access.purrfectly_pawsh.roles.includes("ADMIN"),
+			}
+			setUser(user);
+			} catch (error) {
+				setUser(guest);
+				console.error("Couldn't decode access token")
+			}
+		}
+	}, [auth]);
 
 	useEffect(() => {
 		if (!basketId) {
@@ -34,7 +86,11 @@ export const SessionContextProvider = ({
 	};
 
 	return (
-		<SessionContext.Provider value={{ getBasketId }}>
+		<SessionContext.Provider value={{
+			...auth,
+			getBasketId,
+			user
+		}}>
 			{children}
 		</SessionContext.Provider>
 	);
